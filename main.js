4ebc5821869823c0774655be191450d3016d5c6a
Yure <126098698+yurehfarias@users.noreply.github.com>
2023-04-04 11:34:34 -0300

    Projeto finalizado Alura MIDI

---
+++ main.js
@@ -0,0 +1,38 @@
+function tocaSom (idElementoAudio) {
+  
+  const elemento = document.querySelector(idElementoAudio);
+
+  if(elemento && elemento.localName === 'audio'){
+    elemento.play();
+  }
+  else {
+    console.log('elemento nao encontrado ou seletor invalido')
+  }
+     
+}
+ 
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+
+  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla =listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+      tocaSom(idAudio)
+    }
+    
+    tecla.onkeydown = function (event) {
+
+      console.log(event.code == 'Space')
+      
+      if (event.code === 'Space' || event.code === 'Enter') { 
+      tecla.classList.add('ativa');
+    }
+  
+  }
+    tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+    }
+  }
